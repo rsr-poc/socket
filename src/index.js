6bd5e93288c2d78213bd39ea1dc2b22b5bea5ce3
Bernardo Mafra <bernardo.mafra@sga.pucminas.br>
2022-04-20 11:08:42 -0300

    feat: add dist

--- src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var VideoWatcherNamespace = require("./namespaces/video-watcher");
-var server_1 = require("./setup/server");
-VideoWatcherNamespace.init();
-server_1.server.listen(3000, function () { return console.log("\uD83D\uDD25 Socket listening on port 3000!"); });
--- src/namespaces/video-watcher/events.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.sync = exports.pause = exports.play = void 0;
-var play = function (socket, data) { return console.log('play', data); };
-exports.play = play;
-var pause = function (socket, data) { return console.log('pause', data); };
-exports.pause = pause;
-var sync = function (socket, data) { return console.log('sync', data); };
-exports.sync = sync;
